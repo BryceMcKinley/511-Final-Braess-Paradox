d4b5a4bd04b359abaab8b556a985a13b61a85810
Joseph Giardella <jdgiardella@wpi.edu>
2026-05-03 02:20:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 import random
+import copy
 
 # --- Parameters calibrated for the Paradox ---
 NUM_CARS = 4000 
@@ -71,13 +72,12 @@ class Simulation:
             paths.append([self.roads[0], self.roads[4], self.roads[3]]) # S-A-B-T (The Trap)
         return paths
 
-    def step(self, info=False):
+    def step(self, info=False, avg=True):
         """
         if info:
             SAT = 45 + xa/100
             SBT = 45 + xb/100
-            SABT = (xa + xb)/100
-        """
+            SABT = (xa + xb)/100"""
         
         for r in self.roads: r.cars = 0
         
@@ -94,13 +94,33 @@ class Simulation:
         # Calculating Learning Rate
         car_speeds = [sum(r.travel_time() for r in p) for p in self.car_paths]
         std = np.std(car_speeds)
-        print(std)
         if std > 2:
             LEARNING_RATE = 0.2
         else:
             LEARNING_RATE = std * 0.1
 
         # Drivers switch paths if a better one is found
+        '''
+        attempting to make a code that will only switch cars if it helps the global average. 
+        Not working rn as we are losing cars. Not sure what is wrong
+
+        if avg:
+            temp_car_paths = copy.deepcopy(self.car_paths)
+            for i in range(NUM_CARS):
+                if random.random() < LEARNING_RATE:
+                    hold = temp_car_paths[i]
+                    temp_car_paths[i] = best_path
+            temp_avg = sum(sum(r.travel_time() for r in p) for p in temp_car_paths) / NUM_CARS
+            last_avg = sum(sum(r.travel_time() for r in p) for p in self.car_paths) / NUM_CARS
+            print('temp: ', temp_avg, 'last: ', last_avg)
+            if temp_avg < last_avg:
+                self.car_paths = temp_car_paths
+                print('better')
+            else:
+                print(temp_car_paths[i]==hold)
+                temp_car_paths[i] = hold
+        else:
+        '''
         for i in range(NUM_CARS):
             if random.random() < LEARNING_RATE:
                 self.car_paths[i] = best_path
@@ -109,6 +129,10 @@ class Simulation:
         total_time = sum(sum(r.travel_time() for r in p) for p in self.car_paths)
         return total_time / NUM_CARS
 
+    #the plots are coming back a bit funky cuz I split the right side into two graphs.
+    #this has caused a fantom graph to appear under it with tick mark values.
+    #wasn't able to find a quick fix, idk if you know one.
+    
     def draw_network(self, i, avg_time):
         plt.subplot(1, 2, 1) # Left side: Network
         plt.cla()
@@ -131,6 +155,9 @@ class Simulation:
 # --- Execute ---
 sim = Simulation(include_shortcut=True)
 history = []
+cars_sat = []
+cars_sbt = []
+cars_sabt = []
 
 plt.ion()
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(22, 11), dpi=90)
@@ -139,17 +166,31 @@ plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0.3)
 for i in range(ITERATIONS):
     avg_time = sim.step()
     history.append(avg_time)
+    cars_sat.append(sim.roads[2].cars)
+    cars_sbt.append(sim.roads[1].cars)
+    cars_sabt.append(sim.roads[0].cars - sim.roads[2].cars)
     
     if i % 1 == 0:
         sim.draw_network(i, avg_time)
         
-        # Right side: Equilibrium Graph
-        plt.subplot(1, 2, 2)
+        # Right top: Equilibrium Graph
+        plt.subplot(2, 2, 2)
         plt.plot(history, color='red', linewidth=2)
         plt.ylim(60, 100)
         plt.title(f"Equilibrium Graph: Avg Time = {avg_time:.2f} min")
         plt.xlabel("Iteration")
         plt.ylabel("Avg Travel Time")
+        plt.grid(True, alpha=0.3)
+        
+        #Right bottom: Number of Cars per Path Graph
+        plt.subplot(2, 2, 4)
+        plt.plot(cars_sat, color='red', linewidth=2)
+        plt.plot(cars_sbt, color='green', linewidth=2)
+        plt.plot(cars_sabt, color='blue', linewidth=2)
+        plt.ylim(0, 4000)
+        plt.title(f"Number of Cars Per Path Graph: Total Cars = {NUM_CARS} min")
+        plt.xlabel("Iteration")
+        plt.ylabel("Number of Cars Per Path")
         plt.grid(True, alpha=0.3)
         
         plt.tight_layout()
